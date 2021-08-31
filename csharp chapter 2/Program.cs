400bfade16c9885170e037f1764f53aacf00a7c0
Elfin Lundborg <04elluc@skola.lidkoping.se>
2021-08-31 08:32:39 +0200

    Add project files.

---
+++ csharp chapter 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_chapter_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
